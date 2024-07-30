04f7e2bedcbe87b3c3b91a2b1afce59006eca542
SubhankarRay-192 <subhankarraypp32@gmail.com>
2024-07-31 02:38:09 +0530

    Challenger Buffer Accepted

--- lichess_bot/index.js
+++ lichess_bot/index.js
@@ -1,27 +1,39 @@
 console.log("Hello World!");
 
-const fetch = require('node-fetch');
-
+const https = require('node:https');
 require('dotenv').config();
 
-const BASE_URL = 'https://lichess.org';
 const {
 	BOT_TOKEN,
 } = process.env;
 
 async function listenChallenge(){
-	const res = await fetch(`${BASE_URL}/api/stream/event`, {
-		headers: {
-			Authorization: `Bearer ${BOT_TOKEN}`, 
-		},
-	});
-	const reader = res.body.getReader();
+	
+	const https = require('node:https');
 
-	while(true) {
-		const { value, done } = await reader.read();
-		if(done) break;
-		console.log('Received:', value);
-	}
+	const options = {
+  		hostname: 'lichess.org',
+  		port: 443,
+  		path: '/api/stream/event',
+  		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${BOT_TOKEN}`,
+		},
+	};
+
+	const req = https.request(options, (res) => {
+  	console.log('statusCode:', res.statusCode);
+  	console.log('headers:', res.headers);
+
+  	res.on('data', (data) => {
+    		console.log(data);
+  		});
+	});
+
+	req.on('error', (e) => {
+  		console.error(e);
+	});
+	req.end()
 }
 
 function init_bot(){
