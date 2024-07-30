237bf77f12d05c9fdb30c9c3fecd5d456b4c53a9
SubhankarRay-192 <subhankarraypp32@gmail.com>
2024-07-31 03:07:53 +0530

    Added GET request to lichess

--- lichess_bot/script.js
+++ lichess_bot/script.js
@@ -1,1 +1,29 @@
+<<<<<<< HEAD
 console.log("Hello World!")
+=======
+console.log("Hello World!");
+
+const BASE_URL = 'https://lichess.org';
+const BOT_TOKEN = 'lip_Ehh4RYcoZ3LLQNF9svTI'
+
+function listenChallenge(){
+	const res = await fetch(`${BASE_URL}/api/stream/event`, {
+		header: {
+			Authorization: `Bearer ${BOT_TOKEN}`, 
+		},
+	});
+	const reader = res.body.getReader();
+
+	while(true) {
+		const { value, done } = await reader.read();
+		if(done) break;
+		console.log('Received', value);
+	}
+}
+
+function init_bot(){
+	listenChallenges();
+}
+
+init_bot()
+>>>>>>> 9be567c (Added GET request to lichess)
